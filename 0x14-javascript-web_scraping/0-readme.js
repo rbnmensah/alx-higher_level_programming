c07e5e2b16a1cc10ff9213b48feb21abeaa7258f
Reuben Mensah <rbnmensah@gmail.com>
2024-01-04 02:10:56 +0000

    newFile

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,5 @@
 #!/usr/bin/node
-const process = require('process');
-const filesystem = require('fs');
-
-let file = process.argv[2];
-
-filesystem.readFile(file, 'utf8', function (err, data) {
-  if (err != null) {
-    console.log(err);
-  } else {
-    process.stdout.write(data);
-  }
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
 });
